77089b43a4be19f9f55a54155a12a257ddbf248e
= <=>
2020-03-06 22:26:12 +0700

    test 2

--- resources/views/admin/pages/product/add.blade.php
+++ resources/views/admin/pages/product/add.blade.php
@@ -5,7 +5,7 @@ Thêm loại sản phẩm
 @endsection
 
 @section('content')
-aaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+aaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbb
 <div class="card shadow mb-4">
 	<div class="card-header py-3">
 		<h6 class="m-0 font-weight-bold text-primary">Thêm sản phẩm</h6>
